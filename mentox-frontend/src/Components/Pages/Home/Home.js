63fcaf9c62f826e7f87ec681ab9a6f969a095dca
Shivam Singh <63536484+ShivamSingh22@users.noreply.github.com>
2021-08-13 19:55:08 +0530

    content-pageFix

--- mentox-frontend/src/Components/Pages/Home/Home.js
+++ mentox-frontend/src/Components/Pages/Home/Home.js
@@ -3,7 +3,7 @@ import '../../../App.css';
 import Footer from '../../Footer';
 import HeroSection from '../../HeroSection';
 import Navbar from '../../Navbar';
-import Content from './Content';
+//import Content from './Content';
 import Blog from './index';
 export default function Home(){
     return (<>
