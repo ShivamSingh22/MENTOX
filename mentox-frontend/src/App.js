507fa86e38550ef7cf471c876e1480ce8c16fbf3
Shivam Singh <63536484+ShivamSingh22@users.noreply.github.com>
2021-08-13 22:28:11 +0530

    logIn path updated

--- mentox-frontend/src/App.js
+++ mentox-frontend/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Components/Pages/Home/Home';
-import SignUp from './Components/Pages/SignUp';
+import LogIn from './Components/login';
 import AboutUs from './Components/Pages/AboutUs';
 import Blog from './Components/Pages/Blog';
 import ContactUs from './Components/Pages/ContactUs';
@@ -18,7 +18,7 @@ function App() {
         
        <Route path="/Home" exact component={Home}></Route>
        <Route path="/" exact component={Home}></Route>
-       <Route path="/Sign-up" exact component={SignUp}></Route>
+       <Route path="/Login" exact component={LogIn}></Route>
        <Route path="/About-us" exact component={AboutUs}></Route>
        <Route path="/Blog" exact component={Blog}></Route>
        <Route path="/Contact-us" exact component={ContactUs}></Route>
