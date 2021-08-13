bcf17af8d17c1150c4c2d2db32f54b18f005c93e
Shivam Singh <63536484+ShivamSingh22@users.noreply.github.com>
2021-08-13 22:30:20 +0530

    login page update

--- mentox-frontend/src/Components/Button.js
+++ mentox-frontend/src/Components/Button.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 
 export function Button() {
   return (
-    <Link to='sign-up'>
-      <button className='btn' size="lg">Sign Up</button>
+    <Link to='login'>
+      <button className='btn' size="lg">LogIn</button>
     </Link>
   );
 }
