4ecc243f22936f7f2b7ec8471939cc8194c86566
VDA-001 <vdavishu@gmail.com>
2021-08-14 15:15:24 +0530

    Updated backend with patient form

--- mentox-frontend/src/Components/Button.js
+++ mentox-frontend/src/Components/Button.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Button.css";
 import { Link } from "react-router-dom";
-import { IsAuthenticated, signout } from "../helper/authHelper";
 
 export function Button() {
   return (
--- mentox-frontend/src/Components/login.js
+++ mentox-frontend/src/Components/login.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { Redirect } from "react-router";
+import React from "react";
 import {
   Grid,
   Paper,
@@ -9,85 +8,9 @@ import {
   Typography,
   Link,
 } from "@material-ui/core";
-import { Alert } from "@material-ui/lab";
-import { Container } from "@material-ui/core";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import { signin, Authenticat, IsAuthenticated } from "../helper/authHelper";
 
 const Login = () => {
-  const [values, setValues] = useState({
-    email: "",
-    username: "",
-    password: "",
-    error: "",
-    success: false,
-    didRedirect: false,
-  });
-
-  const { email, username, password, error, success } = values;
-
-  const handleChange = (name) => (event) => {
-    setValues({ ...values, error: false, [name]: event.target.value });
-  };
-
-  //Function to handle login
-  const onSubmit = (event) => {
-    event.preventDefault();
-    setValues({ ...values, error: false });
-    //Check if all fields are filled
-    if (email === "" || username === "" || password === "") {
-      setValues({
-        ...values,
-        error: "Please fill in all the details",
-      });
-    } else {
-      signin({ email, username, password })
-        .then((data) => {
-          if (data.token) {
-            //Setting up the jwt in localstorage
-            Authenticat(data, () => {
-              console.log("Tokken added");
-              setValues({
-                ...values,
-                didRedirect: true,
-              });
-            });
-          } else {
-            console.log("dataerroer");
-            setValues({ ...values, error: "Something went wrong" });
-          }
-        })
-        .catch((err) => console.log(err));
-    }
-  };
-
-  //Display success message using state variable
-  const successMessage = () => {
-    return (
-      <div style={{ display: success ? "" : "none" }}>
-        New account created successfully
-      </div>
-    );
-  };
-
-  //Display error message using state variable
-  const errorMessage = () => {
-    return (
-      <Container>
-        <Alert variant={"danger"} style={{ display: error ? "" : "none" }}>
-          {error}
-        </Alert>
-      </Container>
-    );
-  };
-
-  //Performing redirect after successfull login
-  const performRedirect = () => {
-    if (IsAuthenticated()) {
-      return <Redirect to="/" />;
-    }
-  };
-
   const paperStyle = {
     padding: 20,
     height: "70vh",
@@ -101,9 +24,6 @@ const Login = () => {
 
   return (
     <>
-      {successMessage()}
-      {errorMessage()}
-      {performRedirect()}
       <Grid>
         <Paper elevation={10} style={paperStyle}>
           <Grid align="center">
@@ -112,31 +32,18 @@ const Login = () => {
             </Avatar>
             <h2>Log In</h2>
           </Grid>
-          <TextField
-            label="Username"
-            placeholder="Enter username"
-            fullWidth
-            value={username}
-            required
-            onChange={handleChange("username")}
-          />
+          <TextField label="Username" placeholder="Enter username" fullWidth />
           <TextField
             label="Email"
             placeholder="Enter email"
             type="email"
             fullWidth
-            required
-            value={email}
-            onChange={handleChange("email")}
           />
           <TextField
             label="Password"
             placeholder="Enter password"
             type="password"
             fullWidth
-            value={password}
-            required
-            onChange={handleChange("password")}
           />
           <br />
           <br />
@@ -147,7 +54,6 @@ const Login = () => {
             variant="contained"
             style={btnstyle}
             fullWidth
-            onClick={onSubmit}
           >
             Sign in
           </Button>
